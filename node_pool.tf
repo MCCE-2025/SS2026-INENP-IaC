a0ff29696cac22540d378431c4054a9aba9e0a4f
Michael Lang <20258019@htl.moedling.at>
2026-06-04 21:48:07 +0200

    feat: add initial GKE platform terraform stack in HA mode (#10)

---
+++ node_pool.tf
@@ -0,0 +1,42 @@
+data "google_container_engine_versions" "versions" {}
+
+resource "google_service_account" "service_account" {
+  account_id   = "platform-${var.project_id}"
+  display_name = "Service Account for the demo cluster"
+}
+
+resource "google_container_node_pool" "node_pool" {
+  name_prefix    = "platform-"
+  cluster        = google_container_cluster.cluster.name
+  location       = var.zone
+  node_locations = [var.zone]
+
+  version            = data.google_container_engine_versions.versions.release_channel_default_version["STABLE"]
+  initial_node_count = 1
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 2
+  }
+
+  node_config {
+    spot         = true
+    machine_type = "e2-medium"
+
+    boot_disk {
+      disk_type = "pd-balanced"
+      size_gb   = 25
+    }
+
+    service_account = google_service_account.service_account.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
+
+    labels = {
+      role = "worker"
+    }
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,18 @@
+output "project_id" {
+  value       = var.project_id
+  description = "Project ID where the cluster is deployed"
+}
+output "zone" {
+  value       = var.zone
+  description = "Zone where the cluster is deployed"
+}
+
+output "kubernetes_cluster_name" {
+  value       = google_container_cluster.cluster.name
+  description = "Cluster Name"
+}
+
+output "kubernetes_cluster_gcloud_command" {
+  value       = "KUBECONFIG=./gke.kubeconfig gcloud container clusters get-credentials ${google_container_cluster.cluster.name} --zone ${var.zone} --project ${var.project_id}"
+  description = "Command to get credentials for the cluster"
+}
