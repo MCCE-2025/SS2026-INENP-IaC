7023a67ccb844836bf718c5b971536e4a0fa35ec
Kukurijek <nemanjaf1994@yahoo.com>
2026-06-22 15:41:41 +0200

    fix: fix network for cloud sql

--- cloud-sql-networking.tf
+++ cloud-sql-networking.tf
@@ -4,25 +4,7 @@ resource "google_project_service" "sqladmin" {
   disable_on_destroy = false
 }
 
-resource "google_project_service" "servicenetworking" {
-  project            = var.project_id
-  service            = "servicenetworking.googleapis.com"
-  disable_on_destroy = false
-}
-
-# Reserved range for Cloud SQL private IP peering into the cluster VPC.
-resource "google_compute_global_address" "cloud_sql_private_ip" {
-  name          = "cloud-sql-private-ip"
-  purpose       = "VPC_PEERING"
-  address_type  = "INTERNAL"
-  prefix_length = 16
-  network       = google_compute_network.vpc.id
-}
-
-resource "google_service_networking_connection" "cloud_sql_private_vpc" {
-  network                 = google_compute_network.vpc.id
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.cloud_sql_private_ip.name]
-
-  depends_on = [google_project_service.servicenetworking]
-}
+# Cloud SQL uses a public IP with no authorized networks. Pods connect only via
+# the Cloud SQL Auth Proxy sidecar (see backend Helm chart). Private IP would
+# require google_service_networking_connection, which needs
+# roles/servicenetworking.networksAdmin on the Terraform runner.
